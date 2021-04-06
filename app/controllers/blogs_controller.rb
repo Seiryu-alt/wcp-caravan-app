7c58697123f6dc9986f939a507dad2b17020520f
Seiryu-alt <seiryusain@gmail.com>
2021-04-06 12:52:05 +0000

    [Add] Gem:pry-byebugを追加（開発環境）

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,12 +1,12 @@
-class BlogsController < ApplicationController 
+class BlogsController < ApplicationController
   def show
     @blog = Blog.find(params[:id])
   end
-  
+
   def index
     @blogs = Blog.all
   end
-  
+
   def new
     @blog = Blog.new
   end
